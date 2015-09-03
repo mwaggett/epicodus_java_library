30e29e07e9bec71b320bab41cebdc3969ce632ba
Molly Waggett & Maggie ONeill <student@epicodus.com>
2015-09-03 10:50:51 -0700

    Create web interface homepage and admin home with ability to search for books by title or author.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,19 +18,65 @@ public class App {
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+
+      List<Patron> patrons = Patron.all();
+
+      model.put("patrons", patrons);
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    // post("/newpage", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //
-    //   <Class> param = request.queryParams("param");
-    //   model.put("param", param);
-    //
-    //   model.put("template", "templates/newpage.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    get("/admin", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      List<Book> books = Book.all();
+      model.put("books", books);
+      model.put("template", "templates/admin.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    post("/admin/title/search", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      String searchTitle = request.queryParams("booktitle");
+      List<Book> searchResults = Book.search(searchTitle);
+
+      model.put("books", searchResults); //books here refers to books from search result, 
+      model.put("template", "templates/admin.vtl");//use same template as the admin search page, itll look similar but be a different page
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    post("/admin/author/search", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      String searchAuthor = request.queryParams("bookauthor");
+      List<Author> authorResults = Author.search(searchAuthor);
+      ArrayList<Book> searchResults = new ArrayList<Book>();
+      for (Author author : authorResults) {//for each author this search returns
+        List<Book> authorBooks = author.getBooks();//get which books they've written
+        for (Book book : authorBooks) {//for each book they've written
+          searchResults.add(book);//add the book to the search results array
+        }
+      }
+
+      model.put("books", searchResults); //books here refers to books from search result
+      model.put("template", "templates/admin.vtl");//use same template as the admin search page, itll look similar but be a different page
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    post("/patrons/create", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      String addName = request.queryParams("name");
+
+      Patron newPatron = new Patron(addName);
+      newPatron.save();
+
+      response.redirect("/");
+      return null;
+    });
 
   }
 
